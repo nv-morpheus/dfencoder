866a9de453e4e88cab5b4909b1837edf313c3ae5
Eli Fajardo <efajardo@nvidia.com>
2022-10-13 17:50:23 +0000

    add option to preset categories

--- dfencoder/autoencoder.py
+++ dfencoder/autoencoder.py
@@ -165,6 +165,7 @@ class AutoEncoder(torch.nn.Module):
                  progress_bar=True,
                  n_megabatches=1,
                  scaler='standard',
+                 preset_cats=None,
                  *args,
                  **kwargs):
         super(AutoEncoder, self).__init__(*args, **kwargs)
@@ -180,6 +181,7 @@ class AutoEncoder(torch.nn.Module):
         self.encoder_dropout = encoder_dropout
         self.decoder_dropout = decoder_dropout
         self.min_cats = min_cats
+        self.preset_cats = preset_cats
         self.encoder = []
         self.decoder = []
         self.train_mode = self.train
@@ -330,8 +332,11 @@ class AutoEncoder(torch.nn.Module):
         self.bin_names = list(self.binary_fts.keys())
 
     def init_features(self, df):
+        if self.preset_cats is not None:
+            self.categorical_fts = self.preset_cats
+        else:
+            self.init_cats(df)
         self.init_numeric(df)
-        self.init_cats(df)
         self.init_binary(df)
 
     def build_inputs(self):
